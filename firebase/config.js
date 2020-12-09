cde56c61785b49494e457a5ff35f0a9155d5f5f6
Lirico <dominguezmatiasadrian@gmail.com>
2020-12-08 23:01:28 -0300

    version 4

---
+++ firebase/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBgbnFiD5dBDxrlTjCenheD0bnPQvK4Sdc",
+    authDomain: "product-hunt-639cf.firebaseapp.com",
+    projectId: "product-hunt-639cf",
+    storageBucket: "product-hunt-639cf.appspot.com",
+    messagingSenderId: "46782997509",
+    appId: "1:46782997509:web:3432dec528aa402c3fa86c"
+  };
+
+  export default firebaseConfig;
--- pages/crear-cuenta.js
+++ pages/crear-cuenta.js
@@ -1,26 +1,37 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import { css } from '@emotion/react';
+import Router from 'next/router';
 import Layout from '../components/layout/Layout';
 import { Formulario, Campo, InputSubmit, Error } from '../components/ui/Formulario';
+
+import firebase from '../firebase';
 
 // validaciones
 import useValidacion from '../hooks/useValidacion';
 import validarCrearCuenta from '../validacion/validarCrearCuenta';
 
+const STATE_INICIAL = {
+  nombre: '',
+  email: '',
+  password: ''
+}
+
 const CrearCuenta = () => {
 
-  const STATE_INICIAL = {
-    nombre: '',
-    email: '',
-    password: ''
-  }
+  const [ error, guardarError ] = useState(false);
 
   const { valores, errores, handleSubmit, handleChange, handleBlur} = useValidacion(STATE_INICIAL, validarCrearCuenta, crearCuenta);
 
   const { nombre, email, password } = valores;
 
-  function crearCuenta() {
-    console.log('Creando cuenta...');
+  async function crearCuenta() {
+    try {
+      await firebase.registrar(nombre, email, password);
+      Router.push('/');
+    } catch (error) {
+      console.error('Hubo un error al crear el usuario', error.message);
+      guardarError(error.message);
+    }
   } 
 
   return (
@@ -81,6 +92,8 @@ const CrearCuenta = () => {
             </Campo>
 
             {errores.password && <Error>{errores.password}</Error>} 
+
+            { error && <Error>{error}</Error> }
 
             <InputSubmit 
                 type="submit"
